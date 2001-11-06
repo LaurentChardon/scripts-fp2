d315dae0958407d5952be3045509de8cc66d51ee
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2001-11-06 20:59:13 +0000

    Move GetDBHandle to database.php Add test_port_save

---
+++ test_port_save.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+
+use strict;
+use DBI;
+use element;
+use category;
+use port;
+use database;
+
+use File::Basename;
+use Sys::Syslog;
+
+require config;
+
+my ($dbh, $port, $name);
+
+$dbh = FreshPorts::Database::GetDBHandle();
+
+$name = "security/acid";
+
+print "CREATING port = '$name'\n";
+
+
+# get the element corresponding to this port
+my $element = FreshPorts::Element->new($dbh);
+$element->{pathname} = "/ports/$name";
+
+my $element_id = $element->FetchByName();
+
+if (!$element_id) {
+	# create the element
+	$element_id = $element->save;
+}
+
+my $category = FreshPorts::Category->new($dbh);
+$category->{name} = File::Basename::dirname($name);
+my $category_id = $category->FetchByName();
+if (!$category_id) {
+	$category_id->save;
+}
+
+
+$port = FreshPorts::Port->new($dbh);
+$port->{element_id}  = $element_id;
+$port->{category_id} = $category_id;
+
+print "about to save\n";
+
+my $id = $port->save();
+
+if (!$id) {
+	print "no id found....\n";
+}
+
+print "AFTER CREATION\n";
+
+print "id                   = $port->{id}\n";
+print "element_id           = $port->{element_id}\n";
+print "category_id          = $port->{category_id}\n";
+
+
+
+print "\nAFTER READING IT BACK IN\n";
+$port = FreshPorts::Port->new($dbh);
+$port->{id} = $id;
+
+$port->FetchByID();
+
+print "id                   = $port->{id}\n";
+print "element_id           = $port->{element_id}\n";
+print "category_id          = $port->{category_id}\n";
+
+$dbh->commit();
+$dbh->disconnect();
